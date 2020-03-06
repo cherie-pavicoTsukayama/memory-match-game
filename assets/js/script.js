ee19eaeb57fe41c9d220244cc9ccc3fdfbf20d63
Cherie Pavico-Tsukayama <cherietsukayama@gmail.com>
2020-03-06 12:13:32 -0800

    updated js to match master

--- assets/js/script.js
+++ assets/js/script.js
@@ -38,8 +38,8 @@ for (var i = 0; i < 18; i++) {
 
 
 
-function handleClick(event){
-    if(event.target.className.indexOf("card-back") === -1){
+function handleClick(event) {
+    if (event.target.className.indexOf("card-back") === -1) {
         return;
     }
     var clickedElement = event.target;
@@ -53,14 +53,14 @@ function handleClick(event){
         secondCardClicked = clickedElement;
         secondCardClasses = secondCardClicked.previousElementSibling.className;
         gameCards.removeEventListener('click', handleClick);
-        if (firstCardClasses === secondCardClasses){
+        if (firstCardClasses === secondCardClasses) {
             gameCards.addEventListener('click', handleClick);
             firstCardClicked = null;
             secondCardClicked = null;
             matches++;
             attempts++;
             displayStats();
-            if(matches === maxMatches){
+            if (matches === maxMatches) {
                 document.querySelector(".modal-container").classList.remove('hidden');
             }
         } else {
@@ -81,14 +81,14 @@ function removeHidden() {
     displayStats();
 }
 
-function displayStats(){
+function displayStats() {
     document.getElementById('gamesPlayed').textContent = gamesPlayed;
     document.getElementById('attempts').textContent = attempts;
     document.getElementById('accuracy').textContent = calculateAccuracy(attempts, matches);
 }
 
-function calculateAccuracy(attempts, matches){
-    if(!attempts){
+function calculateAccuracy(attempts, matches) {
+    if (!attempts) {
         return "0%"
     }
     var accuracy = matches / attempts;
@@ -110,7 +110,7 @@ function resetGame() {
 
 function resetCards() {
     var hiddenCards = document.querySelectorAll('.card-back');
-    for(var i = 0; i < hiddenCards.length; i++){
+    for (var i = 0; i < hiddenCards.length; i++) {
         hiddenCards[i].classList.remove('hidden');
     }
 }
@@ -121,13 +121,13 @@ button.addEventListener('click', resetGame);
 function shuffleCards() {
     var allFrontCards = document.querySelectorAll('.card-front');
     var newClassArray = shuffleClasses();
-    for(var i = 0; i < allFrontCards.length; i++){
+    for (var i = 0; i < allFrontCards.length; i++) {
         allFrontCards[i].className = "null";
         allFrontCards[i].className = "card-front " + newClassArray[i];
     }
 }
 
-function shuffleClasses(){
+function shuffleClasses() {
     var logoClasses = ['css-logo',
         'docker-logo',
         'gitHub-logo',
